4dd244e4d1a08a3ed6052af7a0ba7e1417031a83
KonstantoJr <konstantokon29@gmail.com>
2022-12-02 07:11:53 +0200

    done

--- Day_02/part1.py
+++ Day_02/part1.py
@@ -1,3 +1,14 @@
 with open('input.txt', 'r') as f:
+    points = {'X': 1, 'Y': 2, 'Z': 3}
+    win = {'X': 'C', 'Y': 'A', 'Z': 'B'}
+    draw = {'X': 'A', 'Y': 'B', 'Z': 'C'}
+    totalScore = 0
     for line in f:
-        print(line)
+        elf, me = line.split()
+        if win[me] == elf:
+            totalScore += points[me] + 6
+        elif draw[me] == elf:
+            totalScore += points[me] + 3
+        else:
+            totalScore += points[me]
+    print(totalScore)
--- Day_02/part2.py
+++ Day_02/part2.py
@@ -1,3 +1,14 @@
 with open('input.txt', 'r') as f:
+    win = {'C': 1, 'B': 3, 'A': 2}
+    draw = {'A': 1, 'C': 3, 'B': 2}
+    lose = {'B': 1, 'A': 3, 'C': 2}
+    totalScore = 0
     for line in f:
-        print(line)
+        elf, me = line.split()
+        if me == "X":
+            totalScore += lose[elf]
+        elif me == "Y":
+            totalScore += draw[elf] + 3
+        else:
+            totalScore += win[elf] + 6
+    print(totalScore)
