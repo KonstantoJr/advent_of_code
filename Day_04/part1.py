23b31a59112ee24bcdc4355e1b417014a204636f
KonstantoJr <konstantokon29@gmail.com>
2022-12-04 07:10:44 +0200

    done

--- Day_04/part1.py
+++ Day_04/part1.py
@@ -1,3 +1,13 @@
 with open("input.txt", 'r') as f:
+    pairs = 0
     for line in f:
-        print(line)
+        pair1, pair2 = line.split(',')
+        start1, end1 = pair1.split('-')
+        start2, end2 = pair2.split('-')
+        sub1 = set(range(int(start1), int(end1) + 1))
+        sub2 = set(range(int(start2), int(end2) + 1))
+        if sub1.issubset(sub2):
+            pairs += 1
+        elif sub2.issubset(sub1):
+            pairs += 1
+    print(pairs)
--- Day_04/part2.py
+++ Day_04/part2.py
@@ -1,3 +1,11 @@
 with open("input.txt", 'r') as f:
+    pairs = 0
     for line in f:
-        print(line)
+        pair1, pair2 = line.split(',')
+        start1, end1 = pair1.split('-')
+        start2, end2 = pair2.split('-')
+        sub1 = set(range(int(start1), int(end1) + 1))
+        sub2 = set(range(int(start2), int(end2) + 1))
+        if sub1.intersection(sub2) != set():
+            pairs += 1
+    print(pairs)
