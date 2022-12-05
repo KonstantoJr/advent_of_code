629e5fafb701327b0bfbfba7085b04294185d9f3
KonstantoJr <konstantokon29@gmail.com>
2022-12-05 09:59:02 +0200

    done

--- Day_05/part1.py
+++ Day_05/part1.py
@@ -1,3 +1,51 @@
 with open('input.txt', 'r') as f:
+    stack1 = []
+    stack2 = []
+    stack3 = []
+    stack4 = []
+    stack5 = []
+    stack6 = []
+    stack7 = []
+    stack8 = []
+    stack9 = []
+    stacks = [stack1, stack2, stack3, stack4,
+              stack5, stack6, stack7, stack8, stack9]
+    count = 0
+    temp = ""
     for line in f:
-        print(line)
+        if count < 8:
+            stack = 0
+            for i, char in enumerate(line):
+                temp += char
+                if i % 4 == 3:
+                    stacks[stack].append(temp.strip())
+                    stack += 1
+                    temp = ""
+            stack = 0
+            count += 1
+        elif count == 8:
+            for i in range(9):
+                temp = []
+                for j in stacks[i]:
+                    if j != "":
+                        temp.append(j)
+                stacks[i] = temp
+                print(temp)
+            count += 1
+        elif count >= 9:
+            if "move" in line:
+                temp = line.strip().split(" ")
+                how = temp[1]
+                fromStack = temp[3]
+                toStack = temp[5]
+                for i in range(int(how)):
+                    # print(int(fromStack))
+                    if len(stacks[int(fromStack) - 1]) != 0:
+                        item = stacks[int(fromStack) - 1].pop(0)
+                        stacks[int(toStack) - 1].insert(0, item)
+    ans = ""
+    for i in stacks:
+        ans += i.pop(0)
+    ans = ans.replace("[", "")
+    ans = ans.replace("]", "")
+    print(ans)
--- Day_05/part2.py
+++ Day_05/part2.py
@@ -1,3 +1,67 @@
 with open('input.txt', 'r') as f:
+    stack1 = []
+    stack2 = []
+    stack3 = []
+    stack4 = []
+    stack5 = []
+    stack6 = []
+    stack7 = []
+    stack8 = []
+    stack9 = []
+    stacks = [stack1, stack2, stack3, stack4,
+              stack5, stack6, stack7, stack8, stack9]
+    count = 0
+    temp = ""
     for line in f:
-        print(line)
+        if count < 8:
+            stack = 0
+            for i, char in enumerate(line):
+                temp += char
+                if i % 4 == 3:
+                    stacks[stack].append(temp.strip())
+                    stack += 1
+                    temp = ""
+            stack = 0
+            count += 1
+        elif count == 8:
+            for i in range(9):
+                temp = []
+                for j in stacks[i]:
+                    if j != "":
+                        temp.append(j)
+                temp = temp[::-1]
+                stacks[i] = temp
+                print(temp)
+            count += 1
+        elif count >= 9:
+            if "move" in line:
+                temp = line.strip().split(" ")
+                how = temp[1]
+                fromStack = temp[3]
+                toStack = temp[5]
+
+                if len(stacks[int(fromStack) - 1]) != 0:
+                    if len(stacks[int(fromStack) - 1]) >= int(how):
+                        # print(how, fromStack, toStack)
+                        # print(stacks[int(fromStack) - 1])
+                        # print(stacks[int(toStack) - 1])
+                        item = stacks[int(fromStack) - 1][-1:-(int(how)+1):-1]
+                        for _ in range(int(how)):
+                            stacks[int(fromStack)-1].pop()
+                        stacks[int(toStack) - 1].extend(item[::-1])
+                        # print(item)
+                        # print(stacks[int(toStack) - 1])
+                        # print(stacks[int(fromStack) - 1])
+                    else:
+                        item = stacks[int(fromStack) - 1][::]
+                        stacks[int(toStack) - 1].extend(item)
+                        stacks[int(fromStack) - 1] = []
+    ans = ""
+    for i in stacks:
+        if i != []:
+            ans += i.pop()
+        else:
+            ans += " "
+    ans = ans.replace("[", "")
+    ans = ans.replace("]", "")
+    print(ans)
