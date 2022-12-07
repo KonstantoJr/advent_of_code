11bb55c6f94e178a9ca278077866a5705c84e55b
KonstantoJr <konstantokon29@gmail.com>
2022-12-07 07:53:18 +0200

    done

--- Day_07/part1.py
+++ Day_07/part1.py
@@ -1,3 +1,28 @@
+from collections import defaultdict
+
 with open('input.txt', 'r') as f:
+    currentDir = []
+    structure = defaultdict(int)
     for line in f:
-        print(line)
+        if line[0] == '$':
+            command = line.strip().split(" ")
+            if command[1] == 'cd':
+                if command[2] == '..':
+                    currentDir.pop()
+                else:
+                    currentDir.append(command[2])
+            elif command[1] == 'ls':
+                continue
+        else:
+            if 'dir' in line:
+                continue
+            size, file = line.strip().split(" ")
+            size = int(size)
+            for i, dir in enumerate(currentDir):
+                structure["".join(currentDir[:i]) + dir] += size
+
+totalSum = 0
+for dir in structure:
+    if structure[dir] <= 100000:
+        totalSum += structure[dir]
+print(totalSum)
--- Day_07/part2.py
+++ Day_07/part2.py
@@ -1,3 +1,33 @@
+from collections import defaultdict
+
 with open('input.txt', 'r') as f:
+    currentDir = []
+    structure = defaultdict(int)
     for line in f:
-        print(line)
+        if line[0] == '$':
+            command = line.strip().split(" ")
+            if command[1] == 'cd':
+                if command[2] == '..':
+                    currentDir.pop()
+                else:
+                    currentDir.append(command[2])
+            elif command[1] == 'ls':
+                continue
+        else:
+            if 'dir' in line:
+                continue
+            size, file = line.strip().split(" ")
+            size = int(size)
+            for i, dir in enumerate(currentDir):
+                structure["".join(currentDir[:i]) + dir] += size
+
+disk = 70000000
+free = 30000000
+total = structure['/']
+need = total - (disk - free)
+
+dirTodelete = 1e9
+for dir in structure:
+    if structure[dir] >= need:
+        dirTodelete = min(dirTodelete, structure[dir])
+print(dirTodelete)
