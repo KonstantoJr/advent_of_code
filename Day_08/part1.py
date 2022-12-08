b9036249c60c5bb28286a9de940a3c5bd9566ab0
KonstantoJr <konstantokon29@gmail.com>
2022-12-08 07:44:48 +0200

    done

--- Day_08/part1.py
+++ Day_08/part1.py
@@ -1,3 +1,51 @@
 with open('input.txt', 'r') as f:
+    grid = []
     for line in f:
-        print(line)
+        grid.append([])
+        line = line.strip()
+        for tree in line:
+            grid[-1].append(int(tree))
+
+# print(len(grid))
+# print(len(grid[0]))
+visibleOutside = (len(grid)*len(grid[0])) - \
+    ((len(grid) - 2)*(len(grid[0]) - 2))
+# print(visibleOutside)
+visible = 0
+for i in range(len(grid)):
+    for j in range(len(grid[0])):
+        if i == 0 or j == 0 or i == len(grid) - 1 or j == len(grid[0]) - 1:
+            continue
+        currentHeight = grid[i][j]
+        for k in range(i):
+            if grid[k][j] >= currentHeight:
+                break
+        else:
+            visible += 1
+            continue
+        for k in range(i + 1, len(grid)):
+            if grid[k][j] >= currentHeight:
+                break
+        else:
+            visible += 1
+            continue
+        for k in range(j):
+            if grid[i][k] >= currentHeight:
+                break
+        else:
+            visible += 1
+            continue
+        for k in range(j + 1, len(grid[0])):
+            if grid[i][k] >= currentHeight:
+                break
+        else:
+            visible += 1
+            continue
+
+print(visibleOutside + visible)
+print(visible)
+
+# for i in grid:
+#     for j in i:
+#         print(j, end='')
+#     print()
--- Day_08/part2.py
+++ Day_08/part2.py
@@ -1,3 +1,46 @@
+
 with open('input.txt', 'r') as f:
+    grid = []
     for line in f:
-        print(line)
+        grid.append([])
+        line = line.strip()
+        for tree in line:
+            grid[-1].append(int(tree))
+
+viewingDistace = {}
+scenincScore = {}
+for i in range(len(grid)):
+    for j in range(len(grid[0])):
+        scenincScore[(i, j)] = 1
+        viewingDistace[(i, j)] = [0, 0, 0, 0]
+
+for i in range(len(grid)):
+    for j in range(len(grid[0])):
+        if i == 0 or j == 0 or i == len(grid) - 1 or j == len(grid[0]) - 1:
+            continue
+        currentHeight = grid[i][j]
+        for k in range(i-1, -1, -1):
+            viewingDistace[(i, j)][0] += 1
+            if grid[k][j] >= currentHeight:
+                break
+        for k in range(i + 1, len(grid)):
+            viewingDistace[(i, j)][1] += 1
+            if grid[k][j] >= currentHeight:
+                break
+        for k in range(j - 1, -1, -1):
+            viewingDistace[(i, j)][2] += 1
+            if grid[i][k] >= currentHeight:
+                break
+        for k in range(j + 1, len(grid[0])):
+            viewingDistace[(i, j)][3] += 1
+            if grid[i][k] >= currentHeight:
+                break
+
+
+for i in viewingDistace:
+    for j in range(4):
+        if viewingDistace[i][j] == 0:
+            continue
+        scenincScore[i] *= viewingDistace[i][j]
+
+print(max(scenincScore.items(), key=lambda x: x[1]))
