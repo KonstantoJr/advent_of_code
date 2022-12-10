db149a8cc6bf5af318f9628e80a1db3cd8cefab7
KonstantoJr <konstantokon29@gmail.com>
2022-12-10 07:47:52 +0200

    done

--- Day_10/part1.py
+++ Day_10/part1.py
@@ -1,3 +1,27 @@
+
+
 with open('input.txt', 'r') as f:
+    cycle = 0
+    X = 1
+    importantCycles = [20, 60, 100, 140, 180, 220]
+    signalStrength = []
     for line in f:
-        print(line)
+        line = line.strip()
+        if line == 'noop':
+            cycle += 1
+            if cycle in importantCycles:
+                signalStrength.append(X * cycle)
+            continue
+        command, value = line.split()
+        value = int(value)
+        for i in range(2):
+            if i == 0:
+                cycle += 1
+                if cycle in importantCycles:
+                    signalStrength.append(X * cycle)
+            else:
+                cycle += 1
+                if cycle in importantCycles:
+                    signalStrength.append(X * cycle)
+                X += value
+    print(sum(signalStrength))
--- Day_10/part2.py
+++ Day_10/part2.py
@@ -1,3 +1,44 @@
+def drawPixel(pixels, X, pos):
+    if pos == 0:
+        pos = 40
+    if pos == 1:
+        pixels.append([])
+    pos -= 1
+    if pos in (X-1, X, X+1):
+        pixels[-1].append('#')
+    else:
+        pixels[-1].append('.')
+    return pixels
+
+
 with open('input.txt', 'r') as f:
+    cycle = 1
+    X = 1
+    pixels = []
+    temp = []
+    # importantCycles = [40, 80, 120, 160, 200, 240]
     for line in f:
-        print(line)
+        line = line.strip()
+        pos = (cycle) % 40
+        if line == 'noop':
+            pixels = drawPixel(pixels, X, pos)
+            cycle += 1
+            temp.append((X, pos))
+            continue
+
+        command, value = line.split()
+        value = int(value)
+        for i in range(2):
+            pos = (cycle) % 40
+            if i == 0:
+                pixels = drawPixel(pixels, X, pos)
+                temp.append((X, pos))
+            else:
+                pixels = drawPixel(pixels, X, pos)
+                X += value
+                temp.append((X, pos))
+            cycle += 1
+
+    print(temp)
+    for i in pixels:
+        print(''.join(i))
