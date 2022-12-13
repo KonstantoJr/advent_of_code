6380844bb4a5cdc36ceea8e80670bca7fffe69f2
KonstantoJr <konstantokon29@gmail.com>
2022-12-13 08:08:09 +0200

    done

--- Day_13/part1.py
+++ Day_13/part1.py
@@ -1,3 +1,34 @@
+def isInOrder(packet1, packet2):
+    if type(packet1) == type(packet2) == type(1):
+        if packet1 == packet2:
+            return None
+        return packet1 < packet2
+    if type(packet1) == type(1):
+        packet1 = [packet1]
+    if type(packet2) == type(1):
+        packet2 = [packet2]
+
+    for i, j in zip(packet1, packet2):
+        packet = isInOrder(i, j)
+        if packet is not None:
+            return packet
+    if len(packet1) < len(packet2):
+        return True
+    if len(packet1) > len(packet2):
+        return False
+    return None
+
+
 with open('input.txt', 'r') as f:
-    for line in f:
-        print(line)
+    pairs = []
+    data = f.read().split('\n\n')
+    for packets in data:
+        pair = []
+        for packet in packets.splitlines():
+            pair.append(eval(packet.strip()))
+        pairs.append(tuple(pair))
+    inOrder = []
+    for i, p in enumerate(pairs, 1):
+        if isInOrder(p[0], p[1]):
+            inOrder.append(i)
+    print(sum(inOrder))
--- Day_13/part2.py
+++ Day_13/part2.py
@@ -1,3 +1,52 @@
+class Wrapper:
+    def __init__(self, value):
+        self.value = value
+
+    def __lt__(self, other):
+        return isInOrder(self.value, other.value)
+
+    def __gt__(self, other):
+        return not isInOrder(self.value, other.value)
+
+    def __eq__(self, other):
+        return self.value == other
+
+    def __repr__(self):
+        return f'Wrapper({self.value})'
+
+
+def isInOrder(packet1, packet2):
+    if type(packet1) == type(packet2) == type(1):
+        if packet1 == packet2:
+            return None
+        return packet1 < packet2
+    if type(packet1) == type(1):
+        packet1 = [packet1]
+    if type(packet2) == type(1):
+        packet2 = [packet2]
+
+    for i, j in zip(packet1, packet2):
+        packet = isInOrder(i, j)
+        if packet is not None:
+            return packet
+    if len(packet1) < len(packet2):
+        return True
+    if len(packet1) > len(packet2):
+        return False
+    return None
+
+
 with open('input.txt', 'r') as f:
-    for line in f:
-        print(line)
+    pairs = []
+    data = f.read().split('\n\n')
+    for packets in data:
+        pair = []
+        for packet in packets.splitlines():
+            pair.append(eval(packet.strip()))
+        pairs.append(tuple(pair))
+    packets = [Wrapper([2]), Wrapper([6])]
+    for p in pairs:
+        for i in p:
+            packets.append(Wrapper(i))
+    packets.sort()
+    print((packets.index([2]) + 1) * (packets.index([6]) + 1))
