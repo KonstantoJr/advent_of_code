e12956a5027c05ed83714b83e397f8d0d3db0a2c
KonstantoJr <konstantokon29@gmail.com>
2022-12-15 08:17:16 +0200

    done

--- Day_15/part1.py
+++ Day_15/part1.py
@@ -1,3 +1,35 @@
+def isInRange(x, y, sensors):
+    for (x1, y1, d) in sensors:
+        distance = abs(x - x1) + abs(y - y1)
+        if distance <= d:
+            return False
+    return True
+
+
 with open('input.txt', 'r') as f:
-    for line in f:
-        print(line)
+    data = f.read().splitlines()
+
+sensors = set()
+beacons = set()
+for i in data:
+    sensor, value = i.split(':')
+    x1, y1 = sensor.split(',')
+    x1 = int(x1.split('=')[1])
+    y1 = int(y1.split('=')[1])
+    x2, y2 = value.split(',')
+    x2 = int(x2.split('=')[1])
+    y2 = int(y2.split('=')[1])
+    d = abs(x1-x2) + abs(y1-y2)
+    sensors.add((x1, y1, d))
+    beacons.add((x2, y2))
+
+
+level = 2_000_000
+# level = 10
+notBeacons = 0
+
+for x in range(-10_000_000, 10_000_000):
+    if not isInRange(x, level, sensors) and (x, level) not in beacons:
+        notBeacons += 1
+
+print(notBeacons)
--- Day_15/part2.py
+++ Day_15/part2.py
@@ -1,3 +1,39 @@
+def isInRange(x, y, sensors):
+    for (x1, y1, d) in sensors:
+        distance = abs(x - x1) + abs(y - y1)
+        if distance <= d:
+            return False
+    return True
+
+
 with open('input.txt', 'r') as f:
-    for line in f:
-        print(line)
+    data = f.read().splitlines()
+
+sensors = set()
+beacons = set()
+for i in data:
+    sensor, value = i.split(':')
+    x1, y1 = sensor.split(',')
+    x1 = int(x1.split('=')[1])
+    y1 = int(y1.split('=')[1])
+    x2, y2 = value.split(',')
+    x2 = int(x2.split('=')[1])
+    y2 = int(y2.split('=')[1])
+    d = abs(x1-x2) + abs(y1-y2)
+    sensors.add((x1, y1, d))
+    beacons.add((x2, y2))
+
+checked = 0
+for (sx, sy, d) in sensors:
+    for dx in range(d+2):
+        dy = (d+1) - dx
+
+        for bx, by in [(-1, -1), (-1, 1), (1, -1), (1, 1)]:
+            checked += 1
+            x = sx + (dx * bx)
+            y = sy + (dy * by)
+            if not (0 <= x <= 4_000_000 and 0 <= y <= 4_000_000):
+                continue
+            if isInRange(x, y, sensors):
+                print(x*4_000_000 + y)
+                exit()
