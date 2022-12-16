9817577fc9d937318ae5e38e49321573673fa520
KonstantoJr <konstantokon29@gmail.com>
2022-12-16 15:17:32 +0200

    done

--- Day_16/part1.py
+++ Day_16/part1.py
@@ -1,49 +1,40 @@
+from functools import lru_cache
+
+
 class Pipe:
     def __init__(self, name, rate, lead) -> None:
         self.name = name
         self.rate = rate
         self.lead = lead
-        self.open = False
 
     def __repr__(self) -> str:
         return f'{self.name} {self.rate} {self.lead}'
 
 
-def find_path_to_highest_rate_pipe(pipes, pos):
-    temp = [pipe for pipe in pipes.values() if not pipe.open]
-    temp.sort(key=lambda x: x.rate, reverse=True)
-    print(temp)
-    destination = temp[0]
-    path = dfs(pipes, pos, destination, [pos])
-    print(path)
-    return path[1]
+@lru_cache(maxsize=None)
+def findMaxFlow(pipe, opened, min_left):
+    global pipes
+    if min_left <= 0:
+        return 0
+    best = 0
+    if pipe not in opened:
+        val = (min_left - 1) * pipes[pipe].rate
+        cur_opened = tuple(sorted(opened + (pipe,)))
+        for neighbor in pipes[pipe].lead:
+            if pipe not in opened and val:
+                best = max(best, val + findMaxFlow
+                           (neighbor,
+                            cur_opened, min_left - 2))
+            best = max(best,  findMaxFlow
+                       (neighbor, opened, min_left - 1))
+    else:
+        for neighbor in pipes[pipe].lead:
+            best = max(best, findMaxFlow
+                       (neighbor, opened, min_left - 1))
+    return best
 
 
-def dfs(pipes, pos, destination, path):
-    if pos == destination.name:
-        return path
-    for lead in pipes[pos].lead:
-        if lead in path:
-            continue
-        path.append(lead)
-        result = dfs(pipes, lead, destination, path)
-        if result:
-            return result
-        path.pop()
-    return None
-
-
-def calculate_pressure(pipes):
-    pressure = 0
-    for pipe in pipes.values():
-        if pipe.open:
-            print(pipe.name, end=" ")
-            pressure += pipe.rate
-    print()
-    return pressure
-
-
-with open('test.txt', 'r') as f:
+with open('input.txt', 'r') as f:
     data = f.read().splitlines()
 
 pipes = {}
@@ -55,16 +46,8 @@ for line in data:
     lead = word[9:]
     for i in range(len(lead)):
         lead[i] = lead[i].replace(",", "")
+    # print(name, rate, lead)
     pipes[name] = (Pipe(name, rate, lead))
 
-pos = "AA"
-pressure = 0
-for remaining in range(1, 31):
-    pipe = pipes[pos]
-    if pipe.rate > 0 and not pipe.open:
-        pipe.open = True
-    else:
-        pos = find_path_to_highest_rate_pipe(pipes, pos)
-    print(f'Minute {remaining}')
-    pressure += calculate_pressure(pipes)
-    print(f'move to {pos}')
+
+print(findMaxFlow('AA', (), 30))
--- Day_16/part2.py
+++ Day_16/part2.py
@@ -1,3 +1,78 @@
-with open('input.txt', 'r') as f:
-    for line in f:
-        print(line)
+from functools import lru_cache
+
+
+class Pipe:
+    def __init__(self, name, rate, lead) -> None:
+        self.name = name
+        self.rate = rate
+        self.lead = lead
+
+    def __repr__(self) -> str:
+        return f'{self.name} {self.rate} {self.lead}'
+
+
+@lru_cache(maxsize=None)
+def findMaxFlow(pipe, opened, min_left):
+    global pipes
+    if min_left <= 0:
+        return 0
+    best = 0
+    if pipe not in opened:
+        val = (min_left - 1) * pipes[pipe].rate
+        cur_opened = tuple(sorted(opened + (pipe,)))
+        for neighbor in pipes[pipe].lead:
+            if pipe not in opened and val:
+                best = max(best, val + findMaxFlow
+                           (neighbor,
+                            cur_opened, min_left - 2))
+            best = max(best,  findMaxFlow
+                       (neighbor, opened, min_left - 1))
+    else:
+        for neighbor in pipes[pipe].lead:
+            best = max(best, findMaxFlow
+                       (neighbor, opened, min_left - 1))
+    return best
+
+
+@lru_cache(maxsize=None)
+def findMaxFlowWithElephant(pipe, opened, min_left):
+    global pipes
+    if min_left <= 0:
+        # print(opened)
+        return findMaxFlow('AA', opened, 26)
+
+    best = 0
+    if pipe not in opened:
+        val = (min_left - 1) * pipes[pipe].rate
+        cur_opened = tuple(sorted(opened + (pipe,)))
+        for neighbor in pipes[pipe].lead:
+            if pipe not in opened and val:
+                best = max(best, val + findMaxFlowWithElephant
+                           (neighbor,
+                            cur_opened, min_left - 2))
+            best = max(best,  findMaxFlowWithElephant
+                       (neighbor, opened, min_left - 1))
+    else:
+        for neighbor in pipes[pipe].lead:
+            best = max(best, findMaxFlowWithElephant
+                       (neighbor, opened, min_left - 1))
+    return best
+
+
+with open('test.txt', 'r') as f:
+    data = f.read().splitlines()
+
+pipes = {}
+
+for line in data:
+    word = line.split()
+    name = word[1]
+    rate = int(word[4][5:-1])
+    lead = word[9:]
+    for i in range(len(lead)):
+        lead[i] = lead[i].replace(",", "")
+    # print(name, rate, lead)
+    pipes[name] = (Pipe(name, rate, lead))
+
+
+print(findMaxFlowWithElephant('AA', (), 26))
