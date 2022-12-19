3f1ecbbbd15b069bcef83ab452a494b50435948a
KonstantoJr <konstantokon29@gmail.com>
2022-12-20 01:11:47 +0200

    done

--- Day_18/part1.py
+++ Day_18/part1.py
@@ -1,3 +1,27 @@
 with open('input.txt', 'r') as f:
-    for line in f:
-        print(line)
+    data = f.read().splitlines()
+
+cubeCoords = []
+for line in data:
+    x, y, z = line.split(',')
+    cubeCoords.append((int(x), int(y), int(z)))
+
+
+# find if 2 cubes are adjacent
+def isAdjacent(cube1, cube2):
+    x1, y1, z1 = cube1
+    x2, y2, z2 = cube2
+    if (x1 == x2 and y1 == y2 and abs(z1 - z2) == 1) or \
+       (x1 == x2 and z1 == z2 and abs(y1 - y2) == 1) or \
+       (y1 == y2 and z1 == z2 and abs(x1 - x2) == 1):
+        return True
+    return False
+
+
+totalSides = len(cubeCoords) * 6
+print(totalSides)
+for i, cube1 in enumerate(cubeCoords):
+    for cube2 in cubeCoords[i+1:]:
+        if isAdjacent(cube1, cube2):
+            totalSides -= 2
+print(totalSides)
--- Day_18/part2.py
+++ Day_18/part2.py
@@ -1,3 +1,49 @@
 with open('input.txt', 'r') as f:
-    for line in f:
-        print(line)
+    data = f.read().splitlines()
+
+cubeCoords = set()
+for line in data:
+    x, y, z = line.split(',')
+    cubeCoords.add((int(x), int(y), int(z)))
+
+
+# return the neighbors of a cube
+def getNeighbors(cube):
+    x, y, z = cube
+    return [
+        (x+1, y, z), (x-1, y, z),
+        (x, y+1, z), (x, y-1, z),
+        (x, y, z+1), (x, y, z-1)
+    ]
+
+
+xmin, *_, xmax = sorted(cubeCoords, key=lambda x: x[0])
+ymin, *_, ymax = sorted(cubeCoords, key=lambda x: x[1])
+zmin, *_, zmax = sorted(cubeCoords, key=lambda x: x[2])
+
+outside = set([
+    (xmin[0] - 1, ymin[1]-1, zmin[2]-1),
+    (xmax[0] + 1, ymax[1]+1, zmax[2]+1),
+])
+
+while True:
+    inside = set()
+    for cube in outside:
+        for neighbor in getNeighbors(cube):
+            x, y, z = neighbor
+            if neighbor not in cubeCoords:
+                if xmin[0] - 1 <= x <= xmax[0] + 1 and \
+                   ymin[1] - 1 <= y <= ymax[1] + 1 and \
+                   zmin[2] - 1 <= z <= zmax[2] + 1:
+                    inside.add(neighbor)
+    temp = [cube in outside for cube in inside]
+    if all(temp):
+        break
+    outside |= inside
+
+totalsurfaces = 0
+for cube in cubeCoords:
+    for cube1 in getNeighbors(cube):
+        if cube1 in outside:
+            totalsurfaces += 1
+print(totalsurfaces)
