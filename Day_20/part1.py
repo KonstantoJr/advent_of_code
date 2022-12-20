713cc2f6c03341a2b383989a55eda8d2f771c8f8
KonstantoJr <konstantokon29@gmail.com>
2022-12-20 23:15:54 +0200

    done

--- Day_20/part1.py
+++ Day_20/part1.py
@@ -1,3 +1,32 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+
+
 with open('input.txt', 'r') as f:
-    for line in f:
-        print(line)
+    data = f.read().splitlines()
+
+encrypted = []
+for i in data:
+    encrypted.append(Node(int(i)))
+
+decrypted = encrypted.copy()
+
+for node in encrypted:
+    numberIndex = decrypted.index(node)
+    decrypted.remove(node)
+    newIndex = (numberIndex + node.value) % len(decrypted)
+    if newIndex == 0:
+        newIndex = len(decrypted)
+    decrypted.insert(newIndex, node)
+
+for node in decrypted:
+    if node.value == 0:
+        zeroIndex = decrypted.index(node)
+        break
+
+
+first = (decrypted[(zeroIndex + 1000) % len(decrypted)].value)
+second = (decrypted[(zeroIndex + 2000) % len(decrypted)].value)
+third = (decrypted[(zeroIndex + 3000) % len(decrypted)].value)
+print(first + second + third)
--- Day_20/part2.py
+++ Day_20/part2.py
@@ -1,3 +1,33 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+
+
 with open('input.txt', 'r') as f:
-    for line in f:
-        print(line)
+    data = f.read().splitlines()
+
+decryption_key = 811589153
+encrypted = []
+for i in data:
+    encrypted.append(Node(int(i) * decryption_key))
+
+decrypted = encrypted.copy()
+for _ in range(10):
+    for node in encrypted:
+        numberIndex = decrypted.index(node)
+        decrypted.remove(node)
+        newIndex = (numberIndex + node.value) % len(decrypted)
+        if newIndex == 0:
+            newIndex = len(decrypted)
+        decrypted.insert(newIndex, node)
+
+for node in decrypted:
+    if node.value == 0:
+        zeroIndex = decrypted.index(node)
+        break
+
+
+first = (decrypted[(zeroIndex + 1000) % len(decrypted)].value)
+second = (decrypted[(zeroIndex + 2000) % len(decrypted)].value)
+third = (decrypted[(zeroIndex + 3000) % len(decrypted)].value)
+print(first + second + third)
