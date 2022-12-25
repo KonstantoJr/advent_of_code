3dec7b99f0d9ad47142f4d191360003c8ed2519d
KonstantoJr <konstantokon29@gmail.com>
2022-12-25 20:44:32 +0200

    done with part1 cannot do part2 without getting all the start, back to day 17

--- Day_25/part2.py
+++ Day_25/part2.py
@@ -1,3 +1,42 @@
 with open('input.txt', 'r') as f:
-    for line in f:
-        print(line)
+    data = f.read().splitlines()
+
+
+def Decimal2SNAFU(decimal):
+    SNAFU = ''
+    while decimal > 0:
+        remainder = decimal % 5
+        if remainder == 0:
+            SNAFU += '0'
+        elif remainder == 1:
+            SNAFU += '1'
+        elif remainder == 2:
+            SNAFU += '2'
+        elif remainder == 3:
+            SNAFU += '='
+            decimal += 5
+        elif remainder == 4:
+            SNAFU += '-'
+            decimal += 5
+        decimal = decimal // 5
+    return SNAFU[::-1]
+
+
+def SNAFU2Decimal(SNAFU):
+    SNAFU = list(SNAFU)
+    SNAFU.reverse()
+    decimal = 0
+    for i in range(len(SNAFU)):
+        if SNAFU[i].isdigit():
+            decimal += int(SNAFU[i]) * (5 ** i)
+        elif SNAFU[i] == '-':
+            decimal += - 1 * (5 ** i)
+        elif SNAFU[i] == '=':
+            decimal += - 2 * (5 ** i)
+    return decimal
+
+
+decimals = [SNAFU2Decimal(i) for i in data]
+totalFuel = sum(decimals)
+print(totalFuel, Decimal2SNAFU(totalFuel),
+      SNAFU2Decimal(Decimal2SNAFU(totalFuel)))
