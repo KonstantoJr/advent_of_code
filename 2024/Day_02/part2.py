c22f00d126fae92b96ddd5ce0cb4baa2768662fd
KonstantoJr <konstanto.k@ac.upatras.gr>
2024-12-02 19:05:18 +0200

    solved

--- 2024/Day_02/part2.py
+++ 2024/Day_02/part2.py
@@ -1,5 +1,36 @@
 """Solution for day 2 part 2"""
 
+def check_reports(reports_diffs):
+    for diffs in reports_diffs:
+        if check_safe(diffs):
+            break
+    else:
+        return False
+    return True
+
+
+
+def check_safe(differences):
+    ascending = None
+    first = differences[0]
+
+    if (first > 0) and first in {1, 2, 3}:
+        ascending = True
+    elif (first < 0) and first in {-1, -2, -3}:
+        ascending = False
+    else:
+        return False
+
+    for diff in differences:
+        if ascending:
+            if diff not in {1, 2, 3}:
+                return False
+        else:
+            if diff not in {-1, -2, -3}:
+                return False
+
+    return True
+
 with open('input.txt', 'r', encoding='utf-8') as f:
     data = f.read().splitlines()
 
@@ -10,18 +41,26 @@ for i, d in enumerate(data):
 safe = 0
 
 for report in data:
-    current = None
-    previous = None
-    diff = None
-    diffs = []
-    for number in report:
-        if current is None:
+    current_reports = []
+    for i in range(len(report)):
+        current = None
+        previous = None
+        diff = None
+        diffs = []
+        for j ,number in enumerate(report):
+            if (i == j):
+                continue
+            if current is None:
+                current = int(number)
+                continue
+            previous = current
             current = int(number)
-            continue
-        previous = current
-        current = int(number)
-        diff = current - previous
+            diff = current - previous
 
-        diffs.append(diff)
+            diffs.append(diff)
+        current_reports.append(diffs)
+        print(current_reports)
+    if check_reports(current_reports):
+        safe += 1
 
 print(safe)
