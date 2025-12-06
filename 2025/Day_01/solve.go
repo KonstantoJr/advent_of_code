0d9f84eb055983d92d9c605953242d25b87d6a78
KonstantoJr <29215800+KonstantoJr@users.noreply.github.com>
2025-12-06 14:10:57 +0200

    solved part 1 and need to work on part 2

---
+++ 2025/Day_01/solve.go
@@ -0,0 +1,74 @@
+// AOC Day 1 - 2025
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+// Read Input file
+// Return a list with an object that has a command and a value
+type Instruction struct {
+	Command string
+	Value   int64
+}
+
+func readInputFile(filePath string) ([]Instruction, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var instructions []Instruction
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		var command = string(line[0])
+		var value, _ = strconv.ParseInt(line[1:], 10, 64)
+		instructions = append(instructions, Instruction{Command: command, Value: value})
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return instructions, nil
+}
+
+func part1(instructions []Instruction) {
+	pos := int64(50)
+	// Range 0 - 99
+	ans := int64(0)
+	for _, instr := range instructions {
+		switch instr.Command {
+		case "L":
+			clockwise := instr.Value % 100
+			pos = (pos - clockwise + 100) % 100
+		case "R":
+			clockwise := instr.Value % 100
+			pos = (pos + clockwise) % 100
+		}
+		if pos == 0 {
+			ans += 1
+		}
+	}
+	fmt.Println("Part 1 Answer:", ans)
+
+}
+
+func part2(instructions []Instruction) {
+}
+
+func main() {
+	instructions, err := readInputFile("input.txt")
+	if err != nil {
+		fmt.Println("Error reading input file:", err)
+		return
+	}
+	part1(instructions)
+	part2(instructions)
+}
